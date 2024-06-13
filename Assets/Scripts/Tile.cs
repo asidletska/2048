e5c586583be55aabbd90824358ff48f8f289f3c1
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-06-13 16:20:21 +0300

    Scripts : tile move, tile merge, animation

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,12 +1,14 @@
 using TMPro;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Tile : MonoBehaviour
 {
-    public TileState state {  get; private set; }
+    public TileState state { get; private set; }
     public TileCell cell { get; private set; }
-    public int number {  get; private set; }
+    public int number { get; private set; }
+    public bool locked { get;  set; }
 
     private Image background;
     private TextMeshProUGUI text;
@@ -35,5 +37,45 @@ public class Tile : MonoBehaviour
         this.cell.tile = this;
 
         transform.position = cell.transform.position;
+    }
+    public void MoveTo(TileCell cell)
+    {
+        if (this.cell != null)
+        {
+            this.cell.tile = null;
+        }
+        this.cell = cell;
+        this.cell.tile = this;
+
+        StartCoroutine(Animate(cell.transform.position, false));
+    }
+    public void Merge(TileCell cell)
+    {
+        if (this.cell != null)
+        {
+            this.cell.tile = null;
+        }
+        this.cell = null;
+        cell.tile.locked = true;
+        StartCoroutine(Animate(cell.transform.position, true));
+    }
+    private IEnumerator Animate(Vector3 to, bool merging)
+    {
+        float elapsed = 0f;
+        float duration = 0.1f;
+
+        Vector3 from = transform.position;
+        while (elapsed < duration)
+        {
+            transform.position = Vector3.Lerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = to;
+
+        if (merging)
+        {
+            Destroy(gameObject);
+        }
     }
 }
